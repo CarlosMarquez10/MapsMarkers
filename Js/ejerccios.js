201c4bde676ba998dcf2d5446695da7016a2f7e0
CarlosMarquez10 <carlos27marquez10@gmail.com>
2022-02-11 14:00:55 -0500

    se agrega funcion pintar

--- Js/ejerccios.js
+++ Js/ejerccios.js
@@ -1,5 +1,17 @@
+const colorview = document.querySelector(".colorView");
+const colorblue = document.querySelector("#color1").value;
+
+colorview.addEventListener('click', ()=>{
+    var color = '#ffffff';
+    colorview.id="color1";
+});
+
+
+ 
 let map = L.map('map').setView([7.944311, -72.490174], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+
 var iconoBlue = L.icon({iconUrl: "img/icoblue.svg",iconSize: [30, 40],});
 var iconogreen = L.icon({iconUrl: "img/icogreen.svg",iconSize: [30, 40],});
 var iconoGrenYellow = L.icon({iconUrl: "img/icoGreenYellow.svg",iconSize: [30, 40],});
@@ -28,16 +40,6 @@ L.marker([7.944611,-72.490274],{icon: iconoRed}).addTo(map).bindPopup("CLIENTE: 
 L.marker([7.944711,-72.491074],{icon: iconoYellow}).addTo(map).bindPopup("CLIENTE: de prueba");
 
 
-// const svgMarker = L.icon({
-//     path: d="M266.666,0C174.618,0,100,74.619,100,166.667c0,166.667,166.667,366.667,166.667,366.667s166.667-200,166.667-366.66C433.333,74.619,358.713,0,266.666,0z M266.666,266.667c-55.229,0-100-44.771-100-100c0-55.229,44.771-100,100-100c55.229,0,100,44.771,100,100C366.666,221.896,321.895,266.667,266.666,266.667z",
-//     fillColor: "red",
-//     fillOpacity: 1,
-//     strokeWeight: 0,
-//     rotation: 0,
-//     scale: .055,
-//     anchor: new google.maps.Point(15, 30),
-// });
-  
 
 
 
