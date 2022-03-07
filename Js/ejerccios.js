fb7bce7b0bdc05c6164125b8ff4452a10498402a
CarlosMarquez10 <carlos27marquez10@gmail.com>
2022-03-06 20:03:23 -0500

    se agrega funcion de Objetos

--- Js/ejerccios.js
+++ Js/ejerccios.js
@@ -19,6 +19,7 @@ const color14 = document.querySelector("#color14");
 const color15 = document.querySelector("#color15");
 var textoMasivo = document.querySelector("#btnMasivo");
 const btnBuscar = document.querySelector("#btn_consulta1");
+const btnmostrar = document.querySelector("#select");
 document.getElementById('file').addEventListener('change', readFile, false);
 var datosTextArea = new Array();
 
@@ -101,90 +102,105 @@ var iconox = "";
 // a qui empieza la asignacion del codigo para el cuadro de color.
 
 color1.addEventListener("click", () => {
+  var nomcolor = "Azul";
   colorview.id = "color1";
   iconox = iconoBlue;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color2.addEventListener("click", () => {
+  var nomcolor = "magenta";
   colorview.id = "color2";
   iconox = iconomagenta;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color3.addEventListener("click", () => {
+  var nomcolor = "Verde";
   colorview.id = "color3";
   iconox = iconogreen;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color5.addEventListener("click", () => {
+  var nomcolor = "VerdeClaro";
   colorview.id = "color5";
   iconox = iconoGrenYellow;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color6.addEventListener("click", () => {
+  var nomcolor = "Salmon";
   colorview.id = "color6";
   iconox = iconLightSalmon;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color7.addEventListener("click", () => {
+  var nomcolor = "Gris";
   colorview.id = "color7";
   iconox = iconogrey;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color8.addEventListener("click", () => {
+  var nomcolor = "AzulCielo";
   colorview.id = "color8";
   iconox = iconoLightSkyBlue;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color9.addEventListener("click", () => {
+  var nomcolor = "Naranja";
   colorview.id = "color9";
   iconox = iconoorange;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color10.addEventListener("click", () => {
+  var nomcolor = "Rojo";
   colorview.id = "color10";
   iconox = iconoRed;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color11.addEventListener("click", () => {
+  var nomcolor = "Morado";
   colorview.id = "color11";
   iconox = iconopurple;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color12.addEventListener("click", () => {
+  var nomcolor = "Amarillo";
   colorview.id = "color12";
   iconox = iconoYellow;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color13.addEventListener("click", () => {
+  var nomcolor = "Negro";
   colorview.id = "color13";
   iconox = iconoBlack;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color14.addEventListener("click", () => {
+  var nomcolor = "Naranjado";
   colorview.id = "color14";
   iconox = iconoorange;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 color15.addEventListener("click", () => {
+   var nomcolor = "Blanco";
   colorview.id = "color15";
   iconox = iconoWhite;
-  cambiarColor(iconox);
+  cambiarColor(iconox, nomcolor);
 });
 
 var colorico = iconoBlue; // variable para guardar el color.
+var nombreColor = "Azul";
 
 // cambiar el color de los puntos
 
@@ -216,8 +232,9 @@ btnBuscar.addEventListener('click', () => {
 });
 
 
-function cambiarColor(color) {
-    colorico = color;
+function cambiarColor(color, nomcolor) {
+  colorico = color;
+  nombreColor = nomcolor;
 };
 
 
@@ -276,35 +293,74 @@ function pasardato(dato) {
     for (var i = 1; i < output.length; i++) {
       var latitud = output[0];
       var longitud = output[1];
+      var nom = output[2];
+      var dire = output[3];
+      var dato1 = output[4];
+      var dato2 = output[5];
     }
     for (var j = 0; j < latitud.length; j++) {
       var lat = latitud[j];
       var long = longitud[j];
-      pintar(lat, long);
+      let Nombre = nom[j];
+      let Direccion = dire[j];
+      let datouno = dato1[j];
+      let datodos = dato2[j];
+      pintar(lat, long, Nombre, Direccion, datouno, datodos);
 
     }
   }
   );
 }
 
-function pintar(lati, longi){
+
+function pintar(lati, longi, Nom, Dir, d1, d2) {
 
   class Puntos {
-    constructor(latit, longit) {
+    constructor(latit, longit, Nomb, Dire, dt1, dt2) {
       this.lati = latit;
       this.longi = longit;
+      this.Nombre = Nomb;
+      this.Direcciones = Dire;
+      this.dato1 = dt1;
+      this.dato2 = dt2;
     }
   }
-  var damepuntos = new Puntos(lati, longi);
+  var damepuntos = new Puntos(lati, longi, Nom, Dir, d1, d2);
   var puntox = L.marker([damepuntos.lati, damepuntos.longi], { icon: colorico })
-    .addTo(map);
+    .addTo(map)
+    .bindPopup(`Orden: ${damepuntos.dato2} <br> Ciclo: ${damepuntos.dato1} <br> Nombre: ${damepuntos.Nombre}`);
 
-
+  // .openPopup();
+  
   puntox.on('click', cambiarico);
 
   function cambiarico() {
     // map.removeLayer(this);
     puntox.setIcon(colorico);
-  };
+   
+    var datosguardar = new Object();
+    datosguardar.nombre = damepuntos.Nombre;
+    datosguardar.Direccion = damepuntos.Direcciones;
+    datosguardar.coloricono = nombreColor;
+    console.log(datosguardar);
 
+  };
+  // guardardato(datosguardar);
 }
+
+
+// function guardardato(misdatos){
+
+//   var dato = misdatos;
+ 
+
+//   btnmostrar.addEventListener('click', (dato) =>{
+   
+//     console.log(dato);
+
+//   });
+
+
+// };
+
+
