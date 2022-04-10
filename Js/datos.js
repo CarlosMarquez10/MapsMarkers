f1ad71a72ac29cb65301b84421ccfa067d4980d6
CarlosMarquez10 <carlos27marquez10@gmail.com>
2022-04-10 13:33:20 -0500

    cambios en input y colores

--- Js/datos.js
+++ Js/datos.js
@@ -26,6 +26,19 @@ export const color25 = document.querySelector("#color25");
 export const color26 = document.querySelector("#color26");
 export const color27 = document.querySelector("#color27");
 export const color28 = document.querySelector("#color28");
+export const color29 = document.querySelector("#color29");
+export const color30 = document.querySelector("#color30");
+export const color31 = document.querySelector("#color31");
+export const color32 = document.querySelector("#color32");
+export const color33 = document.querySelector("#color33");
+export const color34 = document.querySelector("#color34");
+export const color35 = document.querySelector("#color35");
+export const color36 = document.querySelector("#color36");
+export const color37 = document.querySelector("#color37");
+export const color38 = document.querySelector("#color38");
+export const color39 = document.querySelector("#color39");
+export const color40 = document.querySelector("#color40");
+export const color41 = document.querySelector("#color41");
 export var btnMasivo = document.querySelector("#btnMasivo");
 export const btnBuscar = document.querySelector("#btn_consulta1");
 export const btnmostrar = document.querySelector("#select");
@@ -180,6 +193,74 @@ export var icoAquamarine = L.icon({
 
 export var icoRosyBrown = L.icon({
   iconUrl: "img/icoRosyBrown.svg",
+  iconSize: [30, 40],
+});
+
+
+// nuevos colores - dos
+
+export var icoAzulUno  = L.icon({
+  iconUrl: "img/icoAzulUno.svg",
+  iconSize: [30, 40],
+});
+
+export var icomora = L.icon({
+  iconUrl: "img/icomora.svg",
+  iconSize: [30, 40],
+});
+
+export var icoverdeUno = L.icon({
+  iconUrl: "img/icoverdeUno.svg",
+  iconSize: [30, 40],
+});
+
+export var icoAzulDos = L.icon({
+  iconUrl: "img/icoAzulDos.svg",
+  iconSize: [30, 40],
+});
+
+export var icoAzulTres = L.icon({
+  iconUrl: "img/icoAzulTres.svg",
+  iconSize: [30, 40],
+});
+
+export var icoNaranjaUno = L.icon({
+  iconUrl: "img/icoNaranjaUno.svg",
+  iconSize: [30, 40],
+});
+
+export var icoNaranjaDos = L.icon({
+  iconUrl: "img/icoNaranjaDos.svg",
+  iconSize: [30, 40],
+});
+
+export var icoverdeDos = L.icon({
+  iconUrl: "img/icoverdeDos.svg",
+  iconSize: [30, 40],
+});
+
+export var icoverdetres = L.icon({
+  iconUrl: "img/icoverdeTres.svg",
+  iconSize: [30, 40],
+});
+
+export var icoRojoUno = L.icon({
+  iconUrl: "img/icoRojoUno.svg",
+  iconSize: [30, 40],
+});
+
+export var icoRojoDos = L.icon({
+  iconUrl: "img/icoRojoDos.svg",
+  iconSize: [30, 40],
+});
+
+export var icoAmarilloDos = L.icon({
+  iconUrl: "img/icoAmarilloDos.svg",
+  iconSize: [30, 40],
+});
+
+export var icoAmarilloUno = L.icon({
+  iconUrl: "img/icoAmarilloUno.svg",
   iconSize: [30, 40],
 });
 
@@ -381,6 +462,100 @@ color28.addEventListener("click", () => {
   iconox = icoRosyBrown;
   cambiarColor(iconox, nomcolor);
 });
+
+color29.addEventListener("click", () => {
+  nomcolor = "AzulUno";
+  colorview.id = "color29";
+  iconox = icoAzulUno;
+  cambiarColor(iconox, nomcolor);
+});
+
+color30.addEventListener("click", () => {
+  nomcolor = "mora";
+  colorview.id = "color30";
+  iconox = icomora;
+  cambiarColor(iconox, nomcolor);
+});
+
+color31.addEventListener("click", () => {
+  nomcolor = "verdeUno";
+  colorview.id = "color31";
+  iconox = icoverdeUno;
+  cambiarColor(iconox, nomcolor);
+});
+
+color32.addEventListener("click", () => {
+  nomcolor = "AzulDos";
+  colorview.id = "color32";
+  iconox = icoAzulDos;
+  cambiarColor(iconox, nomcolor);
+});
+
+color33.addEventListener("click", () => {
+  nomcolor = "AzulTres";
+  colorview.id = "color33";
+  iconox = icoAzulTres;
+  cambiarColor(iconox, nomcolor);
+});
+
+color34.addEventListener("click", () => {
+  nomcolor = "NaranjaUno";
+  colorview.id = "color34";
+  iconox = icoNaranjaUno;
+  cambiarColor(iconox, nomcolor);
+});
+
+color35.addEventListener("click", () => {
+  nomcolor = "NaranjaDos";
+  colorview.id = "color35";
+  iconox = icoNaranjaDos;
+  cambiarColor(iconox, nomcolor);
+});
+
+color36.addEventListener("click", () => {
+  nomcolor = "VerdeDos";
+  colorview.id = "color36";
+  iconox = icoverdeDos;
+  cambiarColor(iconox, nomcolor);
+});
+
+color37.addEventListener("click", () => {
+  nomcolor = "Verdetres";
+  colorview.id = "color37";
+  iconox = icoverdetres;
+  cambiarColor(iconox, nomcolor);
+});
+
+color38.addEventListener("click", () => {
+  nomcolor = "RojoUno";
+  colorview.id = "color38";
+  iconox = icoRojoUno;
+  cambiarColor(iconox, nomcolor);
+});
+
+color39.addEventListener("click", () => {
+  nomcolor = "RojoDos";
+  colorview.id = "color39";
+  iconox = icoRojoDos;
+  cambiarColor(iconox, nomcolor);
+});
+
+color40.addEventListener("click", () => {
+  nomcolor = "AmarilloUno";
+  colorview.id = "color40";
+  iconox = icoAmarilloUno;
+  cambiarColor(iconox, nomcolor);
+});
+
+color41.addEventListener("click", () => {
+  nomcolor = "AmarilloDos";
+  colorview.id = "color41";
+  iconox = icoAmarilloDos;
+  cambiarColor(iconox, nomcolor);
+});
+
+
+
 
 function cambiarColor(color, nomcolor) {
   colorico = color;
